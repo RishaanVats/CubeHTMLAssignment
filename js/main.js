a1a983d3a9be159f4e0c5773bd63a7f967fff7ec
RishaanVats <mishra00730@gmail.com>
2026-01-18 23:28:02 +0530

    day 2 changes, responsive alignment, image carousal container works.

--- js/main.js
+++ js/main.js
@@ -2,58 +2,160 @@
 const images = {
     0: {
         alt: "Main bottle of perfume",
-        address: "./assets/mainPerfume.png"
+        address: "./assets/mainPerfume.png",
     },
     1: {
         alt: "Two bottles of oils",
-        address: "./assets/thumbnails/pexels-pixabay-1.svg"
+        address: "./assets/thumbnails/pexels-pixabay-1.svg",
     },
     2: {
         alt: "One bottle of essential oil",
-        address: "./assets/thumbnails/pexels-pixabay-2.svg"
+        address: "./assets/thumbnails/pexels-pixabay-2.svg",
     },
     3: {
         alt: "Bottles of oils with a diffuser",
-        address: "./assets/thumbnails/pexels-pixabay-3.svg"
+        address: "./assets/thumbnails/pexels-pixabay-3.svg",
     },
     4: {
         alt: "Bottle of oil with flowers",
-        address: "./assets/thumbnails/pexels-pixabay-4.svg"
+        address: "./assets/thumbnails/pexels-pixabay-4.svg",
     },
     5: {
         alt: "Two bottles of oils",
-        address: "./assets/thumbnails/pexels-pixabay-1a.svg"
+        address: "./assets/thumbnails/pexels-pixabay-1a.svg",
     },
     6: {
         alt: "One bottle of essential oil",
-        address: "./assets/thumbnails/pexels-pixabay-2a.svg"
+        address: "./assets/thumbnails/pexels-pixabay-2a.svg",
     },
     7: {
         alt: "Bottles of oils with a diffuser",
-        address: "./assets/thumbnails/pexels-pixabay-3a.svg"
-    }
+        address: "./assets/thumbnails/pexels-pixabay-3a.svg",
+    },
 };
 
-let mainImg = document.getElementById("mainImg"); // ready to set the main image
+var mainImg = document.getElementById("mainImg"); // ready to set the main image
+var imgSet = { id: "", src: "", alt: "" }; // Current main image tracking
+
 //Setting the default main image
-if (!mainImg.src) { mainImg.src = images[0].address; mainImg.alt = images[0].alt; }
+imgSet = { id: "0", src: images[0].address, alt: images[0].alt };
+mainImg.src = images[0].address;
+mainImg.alt = images[0].alt;
+setDots(Number(0));
+
+function setMainImage(id, move) {
+    if (!move) {
+        move = "N/A";
+    }
+    id = Number(id);
+
+    if (move === "left") {
+        id = id === 0 ? 7 : id - 1;
+    } else if (move === "right") {
+        id = id === 7 ? 0 : id + 1;
+    }
+    imgSet = { id: id, src: images[id].address, alt: images[id].alt };
+    mainImg.src = imgSet.src;
+    mainImg.alt = imgSet.alt;
+    setDots(id);
+}
 
 // Render all the thumbnails dynamically
 let thumbnailsHTML = "";
 Object.entries(images).forEach(([Id, element]) => {
-    const thumb = `<img id="image-${Id}" src=${element.address} alt=${element.alt} />`
+    const thumb = `<img id="image-${Id}" src="${element.address}" alt="${element.alt}" />`;
     thumbnailsHTML += thumb;
 });
 document.getElementById("thumbnailImages").innerHTML = thumbnailsHTML;
 
+function setDots() {
+    // Render all the dots dynamically
+    let dotsHTML = "";
+    Object.entries(images).forEach(([Id]) => {
+        if (Number(Id) === Number(imgSet.id)) {
+            console.log("true");
+            var dots = `<img id="dot-${Id}" src="./assets/icons/Dot.png" alt="Dot of selected Image" />`;
+        } else {
+            console.log("false");
+            var dots = `<img id="dot-${Id}" src="./assets/icons/Dot-1.png" alt="Dot of an Image" />`;
+        }
+        dotsHTML += dots;
+    });
+    document.getElementById("dotIndicators").innerHTML = dotsHTML;
+}
+
 // Listen to the clicks on the thumbnails and then set them as the main Image.
-document.getElementById("thumbnailImages").addEventListener("click", function (event) {
-    const clickedOn = event.target;
-    console.log(clickedOn.tagName);
-    const id = clickedOn.id;
-    let numId = id.split("-")[1];
-    if (clickedOn.tagName === "IMG") {
-        mainImg.src = images[numId].address;
-        mainImg.alt = images[numId].alt;
+document
+    .getElementById("thumbnailImages")
+    .addEventListener("click", function (event) {
+        const clickedOn = event.target;
+        const id = clickedOn.id;
+        let numId = id.split("-")[1];
+        if (clickedOn.tagName === "IMG") {
+            mainImg.src = images[numId].address;
+            mainImg.alt = images[numId].alt;
+            imgSet = {
+                id: numId,
+                src: images[numId].address,
+                alt: images[numId].alt,
+            };
+        }
+        setDots(id);
+    });
+
+// Listen to the clicks on the dots and then set them as the main Image.
+document
+    .getElementById("dotIndicators")
+    .addEventListener("click", function (event) {
+        const clickedOn = event.target;
+        const id = clickedOn.id;
+        let numId = id.split("-")[1];
+        if (clickedOn.tagName === "IMG") {
+            mainImg.src = images[numId].address;
+            mainImg.alt = images[numId].alt;
+            imgSet = {
+                id: numId,
+                src: images[numId].address,
+                alt: images[numId].alt,
+            };
+        }
+        setDots(id);
+    });
+
+// When the user clicks on the button, toggle between hiding and showing the dropdown content
+// Hamburger Menu Click and animate
+function hamburgerSwitch(ele) {
+    ele.classList.toggle("change");
+    document.getElementById("menuDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.closest(".containerHam")) {
+        var dropdowns = document.getElementsByClassName("dropdownContent");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains("show")) {
+                openDropdown.classList.remove("show");
+            }
+        }
+        document.querySelector(".containerHam").classList.toggle("change");
     }
-});
+};
+
+var leftArrow = document.getElementById("leftArrow");
+leftArrow.addEventListener("click", () => setMainImage(imgSet.id, "left"));
+
+var rightArrow = document.getElementById("rightArrow");
+rightArrow.addEventListener("click", () => setMainImage(imgSet.id, "right"));
+
+// Add an event listener for the 'resize' event testing
+// window.addEventListener('resize', function () {
+//     const currentWidth = window.innerWidth;
+//     console.log("Window width:", currentWidth, "px");
+
+//     // Optional: display the width on the page
+//     const widthOutput = document.querySelector("body");
+//     console.log("Body width: ", widthOutput.clientWidth, 'px');
+// });
